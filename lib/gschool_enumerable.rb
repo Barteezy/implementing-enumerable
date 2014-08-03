0801154a9d0cdc2f55d0c7d18caa8c5d6670f41e
Luke Bartel <bartelluke@gmail.com>
2014-08-03 12:05:17 -0600

    rewrote enumerable methods

--- lib/gschool_enumerable.rb
+++ lib/gschool_enumerable.rb
@@ -1,3 +1,63 @@
 module GschoolEnumerable
 
+  def g_each
+    for item in self
+      yield(item)
+    end
+    self
+  end
+
+  def g_select
+    ary = []
+    self.g_each do |item|
+      if yield(item)
+        ary << item
+      end
+    end
+    ary
+  end
+
+  def g_reject
+    ary = []
+    self.g_each do |item|
+      unless yield(item)
+        ary << item
+      end
+    end
+    ary
+  end
+
+  def g_map
+    ary = []
+    self.g_each do |item|
+      ary << yield(item)
+    end
+    ary
+  end
+
+  def g_find
+    ary = []
+    self.g_each do |item|
+      if yield(item)
+        ary << item
+      end
+    end
+    ary[0]
+  end
+
+  def g_select!
+    self.g_each do |a|
+      self.delete(a)
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
